abffe034d54b05ac9a26cdbcb0fb3062b34abdc4
AVasilaki <poseydon463@gmail.com>
2023-10-31 12:30:41 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,76 +1,3 @@
-// import { Component } from 'react';
-// import { Searchbar } from './Searchbar/Searchbar';
-
-// import { ImageGallery } from './ImageGallery/ImageGallery';
-// import { Button } from './Button/Button';
-// import { Audio } from 'react-loader-spinner';
-// import { fetchApi } from 'api/api';
-
-// export class App extends Component {
-//   state = {
-//     keyWord: '',
-//     images: [],
-//     page: 1,
-//     loader: false,
-//   };
-
-//   onLoadMore = () => {
-//     this.setState(prevState => ({
-//       page: (prevState.page = prevState.page + 1),
-//       loader: true,
-//     }));
-//     console.log(this.state.page);
-//   };
-
-//   onChange = evt => {
-//     evt.preventDefault();
-//     const form = evt.currentTarget;
-//     this.setState({
-//       keyWord: form.elements.keyword.value,
-//       images: [],
-//       page: 1,
-//     });
-//   };
-
-//   async componentDidUpdate(_, prew) {
-//     const keyWord = this.state.keyWord;
-//     const page = this.state.page;
-
-//     if (prew.keyWord !== keyWord || prew.page !== page) {
-//       this.setState({ loader: true });
-//       console.log(' pre page', prew.page, 'page', page);
-//       try {
-//         const resp = await fetchApi(page, keyWord);
-
-//         this.setState(prevState => ({
-//           images: [...prevState.images, ...resp.data.hits],
-//         }));
-//         console.log(resp);
-//         console.log(page);
-//         return resp;
-//       } catch (error) {
-//         console.error(error);
-//         alert('something wrong');
-//         return error;
-//       } finally {
-//         this.setState({ loader: false });
-//       }
-//     }
-//   }
-//   render() {
-//     return (
-//       <div>
-//         <Searchbar onChange={this.onChange}></Searchbar>
-//         <ImageGallery galery={this.state.images}></ImageGallery>
-//         <Audio visible={this.state.loader} />
-//         <Button
-//           galery={this.state.images.length}
-//           onLoadMore={this.onLoadMore}
-//         ></Button>
-//       </div>
-//     );
-//   }
-// }
 import { useEffect, useState } from 'react';
 import { Button } from './Button/Button';
 import { fetchApi } from 'api/api';
@@ -92,12 +19,16 @@ export const App = () => {
     evt.preventDefault();
     const form = evt.currentTarget;
     setKeyword(form.elements.keyword.value);
+    setImages([]);
+    setPage(1);
   };
   useEffect(() => {
+    if (!keyword) return;
+    setLoader(true);
     async function getI() {
       try {
         const resp = await fetchApi(page, keyword);
-        setImages(resp.data.hits);
+        setImages(p => [...p, ...resp.data.hits]);
         console.log('images', images);
         // return resp;
       } catch (error) {
--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,36 +1,68 @@
+// import { Modalw } from 'components/Modal/Modal';
+// import { Component } from 'react';
+
+// export class ImageGaleryItem extends Component {
+//   state = {
+//     isOpenModal: false,
+//   };
+
+//   onOpenModal = () => {
+//     this.setState({ isOpenModal: true });
+//   };
+
+//   onCloseModal = () => {
+//     this.setState({ isOpenModal: false });
+//   };
+
+//   render() {
+//     const { item } = this.props;
+//     return (
+//       <>
+//         <li className="ImageGalleryItem " onClick={this.onOpenModal}>
+//           <img
+//             className="ImageGalleryItem-image "
+//             src={item.webformatURL}
+//             alt=""
+//           />
+//         </li>
+//         <Modalw
+//           isOpen={this.state.isOpenModal}
+//           isClose={this.onCloseModal}
+//           large={item.largeImageURL}
+//         ></Modalw>
+//       </>
+//     );
+//   }
+// }
 import { Modalw } from 'components/Modal/Modal';
-import { Component } from 'react';
+import { useState } from 'react';
 
-export class ImageGaleryItem extends Component {
-  state = {
-    isOpenModal: false,
+export const ImageGaleryItem = ({ item }) => {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
+  const onOpenModal = () => {
+    setIsOpenModal(true);
   };
 
-  onOpenModal = () => {
-    this.setState({ isOpenModal: true });
+  const onCloseModal = () => {
+    setIsOpenModal(false);
   };
 
-  onCloseModal = () => {
-    this.setState({ isOpenModal: false });
-  };
-
-  render() {
-    const { item } = this.props;
-    return (
-      <>
-        <li className="ImageGalleryItem " onClick={this.onOpenModal}>
-          <img
-            className="ImageGalleryItem-image "
-            src={item.webformatURL}
-            alt=""
-          />
-        </li>
-        <Modalw
-          isOpen={this.state.isOpenModal}
-          isClose={this.onCloseModal}
-          large={item.largeImageURL}
-        ></Modalw>
-      </>
-    );
-  }
-}
+  // const { item } = this.props;
+  return (
+    <>
+      <li className="ImageGalleryItem " onClick={onOpenModal}>
+        <img
+          className="ImageGalleryItem-image "
+          src={item.webformatURL}
+          alt=""
+        />
+      </li>
+      <Modalw
+        isOpen={isOpenModal}
+        isClose={onCloseModal}
+        large={item.largeImageURL}
+      ></Modalw>
+    </>
+  );
+};
