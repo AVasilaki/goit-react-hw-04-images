ec09c8ef41fa463b717fb3c5397b5e2cb2c5df29
AVasilaki <poseydon463@gmail.com>
2023-10-31 13:01:15 +0200

    Update ImageGalleryItem.js

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,39 +1,3 @@
-// import { Modalw } from 'components/Modal/Modal';
-// import { Component } from 'react';
-
-// export class ImageGaleryItem extends Component {
-//   state = {
-//     isOpenModal: false,
-//   };
-
-//   onOpenModal = () => {
-//     this.setState({ isOpenModal: true });
-//   };
-
-//   onCloseModal = () => {
-//     this.setState({ isOpenModal: false });
-//   };
-
-//   render() {
-//     const { item } = this.props;
-//     return (
-//       <>
-//         <li className="ImageGalleryItem " onClick={this.onOpenModal}>
-//           <img
-//             className="ImageGalleryItem-image "
-//             src={item.webformatURL}
-//             alt=""
-//           />
-//         </li>
-//         <Modalw
-//           isOpen={this.state.isOpenModal}
-//           isClose={this.onCloseModal}
-//           large={item.largeImageURL}
-//         ></Modalw>
-//       </>
-//     );
-//   }
-// }
 import { Modalw } from 'components/Modal/Modal';
 import { useState } from 'react';
 
@@ -48,7 +12,6 @@ export const ImageGaleryItem = ({ item }) => {
     setIsOpenModal(false);
   };
 
-  // const { item } = this.props;
   return (
     <>
       <li className="ImageGalleryItem " onClick={onOpenModal}>
