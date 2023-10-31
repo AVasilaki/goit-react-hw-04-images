02c867935a4fc52553e4541ef64c0df2062b3f11
AVasilaki <poseydon463@gmail.com>
2023-10-31 13:05:59 +0200

    upd

--- src/components/App(huks).jsx
+++ src/components/App(huks).jsx
@@ -1,47 +1,47 @@
-import { useEffect, useState } from 'react';
-import { Button } from './Button/Button';
-import { fetchApi } from 'api/api';
-import { Searchbar } from './Searchbar/Searchbar';
-import { Audio } from 'react-loader-spinner';
-import { ImageGallery } from './ImageGallery/ImageGallery';
-export const App = () => {
-  const [keyword, setKeyword] = useState('');
-  const [images, setImages] = useState([]);
-  const [page, setPage] = useState(1);
-  const [loader, setLoader] = useState(false);
+// import { useEffect, useState } from 'react';
+// import { Button } from './Button/Button';
+// import { fetchApi } from 'api/api';
+// import { Searchbar } from './Searchbar/Searchbar';
+// import { Audio } from 'react-loader-spinner';
+// import { ImageGallery } from './ImageGallery/ImageGallery';
+// export const App = () => {
+//   const [keyword, setKeyword] = useState('');
+//   const [images, setImages] = useState([]);
+//   const [page, setPage] = useState(1);
+//   const [loader, setLoader] = useState(false);
 
-  const onLoadMore = () => {
-    setPage((page = page + 1));
-    setLoader(true);
-  };
+//   const onLoadMore = () => {
+//     setPage((page = page + 1));
+//     setLoader(true);
+//   };
 
-  const onChange = evt => {
-    evt.preventDefault();
-    const form = evt.currentTarget;
-    setKeyword((keyword = form.elements.keyword.value));
-  };
-  useEffect(() => {
-    async function getI() {
-      try {
-        const resp = await fetchApi(page, keyword);
-        setImages(resp);
-        return resp;
-      } catch (error) {
-        console.error(error);
-        alert('something wrong');
-        return error;
-      } finally {
-        setLoader(false);
-      }
-    }
-    getI();
-  }, [page, keyword]);
-  return (
-    <div>
-      <Searchbar onChange={onChange}></Searchbar>
-      <ImageGallery galery={images}></ImageGallery>
-      <Audio visible={loader} />
-      <Button galery={images.length} onLoadMore={onLoadMore}></Button>
-    </div>
-  );
-};
+//   const onChange = evt => {
+//     evt.preventDefault();
+//     const form = evt.currentTarget;
+//     setKeyword((keyword = form.elements.keyword.value));
+//   };
+//   useEffect(() => {
+//     async function getI() {
+//       try {
+//         const resp = await fetchApi(page, keyword);
+//         setImages(resp);
+//         return resp;
+//       } catch (error) {
+//         console.error(error);
+//         alert('something wrong');
+//         return error;
+//       } finally {
+//         setLoader(false);
+//       }
+//     }
+//     getI();
+//   }, [page, keyword]);
+//   return (
+//     <div>
+//       <Searchbar onChange={onChange}></Searchbar>
+//       <ImageGallery galery={images}></ImageGallery>
+//       <Audio visible={loader} />
+//       <Button galery={images.length} onLoadMore={onLoadMore}></Button>
+//     </div>
+//   );
+// };
--- src/components/ImageGalleryItem/imgIten(huks).js
+++ src/components/ImageGalleryItem/imgIten(huks).js
@@ -1,32 +1,32 @@
-import { Modalw } from 'components/Modal/Modal';
-import { useState } from 'react';
+// import { Modalw } from 'components/Modal/Modal';
+// import { useState } from 'react';
 
-export const ImageGaleryItem = () => {
-  const [isOpenModal, setIsOpenModal] = useState(false);
+// export const ImageGaleryItem = () => {
+//   const [isOpenModal, setIsOpenModal] = useState(false);
 
-  onOpenModal = () => {
-    setIsOpenModal(true);
-  };
+//   onOpenModal = () => {
+//     setIsOpenModal(true);
+//   };
 
-  onCloseModal = () => {
-    setIsOpenModal(false);
-  };
+//   onCloseModal = () => {
+//     setIsOpenModal(false);
+//   };
 
-  const { item } = this.props;
-  return (
-    <>
-      <li className="ImageGalleryItem " onClick={this.onOpenModal}>
-        <img
-          className="ImageGalleryItem-image "
-          src={item.webformatURL}
-          alt=""
-        />
-      </li>
-      <Modalw
-        isOpen={isOpenModal}
-        isClose={onCloseModal}
-        large={item.largeImageURL}
-      ></Modalw>
-    </>
-  );
-};
+//   const { item } = this.props;
+//   return (
+//     <>
+//       <li className="ImageGalleryItem " onClick={this.onOpenModal}>
+//         <img
+//           className="ImageGalleryItem-image "
+//           src={item.webformatURL}
+//           alt=""
+//         />
+//       </li>
+//       <Modalw
+//         isOpen={isOpenModal}
+//         isClose={onCloseModal}
+//         large={item.largeImageURL}
+//       ></Modalw>
+//     </>
+//   );
+// };
